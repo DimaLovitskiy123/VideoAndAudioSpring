75bd743cfc1838d703d8f583fcdc806e67fe7a3b
ДИМА <dimlovitskiy@gmail.com>
2021-12-14 22:16:54 +0300

    VideoAndAudioSpring

--- src/com/blablabla/Computer.java
+++ src/com/blablabla/Computer.java
@@ -1,15 +1,10 @@
 package com.blablabla;
 
-public class Computer implements IMusicPlayer{
+public class Computer extends MusicPlayer{
     private Music music;
 
     public Computer(Music music) {
         super(music);
-    }
-
-    @Override
-    public void playAudio() {
-
     }
 
     public void playerOn(){
